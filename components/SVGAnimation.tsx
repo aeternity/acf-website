c8542c8c613e408ba4849faa8f02e95f103fb7bd
Rumyana Akat <kerr4o@gmail.com>
2023-08-01 15:03:40 +0300

    Added max size of animations.

--- components/SVGAnimation.tsx
+++ components/SVGAnimation.tsx
@@ -5,12 +5,13 @@ export const SVGAnimation = ({
   data: string;
   className?: string;
 }) => (
-  <div className="relative">
+  <div className="relative left-8 lg:left-0">
     <object
-      className={
-        "svg-animated-bg relative lg:absolute top-0 right-0 overflow-hidden will-change-[transform, opacity, filter] z-0 backface bg-contain bg-right-top w-auto " +
-        className
-      }
+      className={`svg-animated-bg relative lg:absolute
+      max-h-[200px] md:max-h-[300px] lg:max-h-max
+      top-0 right-0 
+      overflow-hidden will-change-[transform, opacity, filter] z-0 
+      backface bg-contain bg-right-top w-auto ${className}`}
       type="image/svg+xml"
       data={data}
     />
